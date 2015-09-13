e205c4e7ec519b91b68d77caf785cc99ad871609
xzzpig <114628972@qq.com>
2015-09-13 13:42:07 +0800

    start Level system

---
+++ src/com/github/xzzpig/morerpg/skills/SkillerExp.java
@@ -0,0 +1,12 @@
+package com.github.xzzpig.morerpg.skills;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class SkillerExp implements Listener{
+	@EventHandler
+	private void onGetExp(EntityDamageByEntityEvent event) {
+		
+	}
+}
